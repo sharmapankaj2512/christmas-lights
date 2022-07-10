92c17d354e955955cd9bc41c00646d972ecbe164
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-07-10 18:50:50 +0400

    two by two grid - turn on single light

--- test/christmas/test_turn_on_lights.rb
+++ test/christmas/test_turn_on_lights.rb
@@ -11,5 +11,14 @@ module Christmas
       assert_equal 1, grid.on_count
       assert_equal 0, grid.off_count
     end
+
+    def test_light_on_two_by_two
+      grid = Grid.new(2)
+      grid.turn_off(0, 1)
+      grid.turn_on(0, 1)
+
+      assert_equal 4, grid.on_count
+      assert_equal 0, grid.off_count
+    end
   end
 end
