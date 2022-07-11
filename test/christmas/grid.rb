d4ecf2d906b43b26fe9f50e9bcac4a5658a8cd38
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-07-11 09:12:16 +0400

    refactor use symbols

--- test/christmas/grid.rb
+++ test/christmas/grid.rb
@@ -7,21 +7,24 @@ module Christmas
     end
 
     def turn_off_range(start, last)
-      apply(start, last, ->(light) { light.turn_off })
+      apply(start, last, :turn_off)
     end
 
     def turn_on_range(start, last)
-      apply(start, last, ->(light) { light.turn_on })
+      apply(start, last, :turn_on)
     end
 
     def toggle_range(start, last)
-      apply(start, last, ->(light) { light.toggle })
+      apply(start, last, :toggle)
     end
 
     def apply(start, last, action)
       x1, y1 = start
       x2, y2 = last
-      (x1..x2).each { |x| (y1..y2).each { |y| action.call(@lights[x][y]) } }
+      (x1..x2).each do |x|
+        (y1..y2).map { |y| @lights[x][y] }
+                .each(&action)
+      end
     end
 
     def on_count
