41416f571f0dc187cf72e734575cc8e076dedfd7
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-07-11 09:23:03 +0400

    refactor rename

--- test/christmas/grid.rb
+++ test/christmas/grid.rb
@@ -6,15 +6,15 @@ module Christmas
       @lights = Array.new(size) { Array.new(size) { Light.new } }
     end
 
-    def turn_off_range(start, last)
+    def turn_off(start, last)
       apply(start, last, :turn_off)
     end
 
-    def turn_on_range(start, last)
+    def turn_on(start, last)
       apply(start, last, :turn_on)
     end
 
-    def toggle_range(start, last)
+    def toggle(start, last)
       apply(start, last, :toggle)
     end
 
@@ -22,8 +22,7 @@ module Christmas
       x1, y1 = start
       x2, y2 = last
       (x1..x2).each do |x|
-        (y1..y2).map { |y| @lights[x][y] }
-                .each(&action)
+        (y1..y2).map { |y| @lights[x][y] }.each(&action)
       end
     end
 
--- test/christmas/test_turn_on_lights.rb
+++ test/christmas/test_turn_on_lights.rb
@@ -5,8 +5,8 @@ module Christmas
   class TestTurnLightsOn < Minitest::Test
     def test_lights_on
       grid = Grid.new(1)
-      grid.turn_off_range([0, 0], [0, 0])
-      grid.turn_on_range([0, 0], [0, 0])
+      grid.turn_off([0, 0], [0, 0])
+      grid.turn_on([0, 0], [0, 0])
 
       assert_equal 1, grid.on_count
       assert_equal 0, grid.off_count
@@ -14,8 +14,8 @@ module Christmas
 
     def test_light_on_two_by_two
       grid = Grid.new(2)
-      grid.turn_off_range([0, 1], [0, 1])
-      grid.turn_on_range([0, 1], [0, 1])
+      grid.turn_off([0, 1], [0, 1])
+      grid.turn_on([0, 1], [0, 1])
 
       assert_equal 4, grid.on_count
       assert_equal 0, grid.off_count
@@ -23,8 +23,8 @@ module Christmas
 
     def test_lights_on_two_by_two_horizontal_line
       grid = Grid.new(2)
-      grid.turn_off_range([0, 0], [1, 1])
-      grid.turn_on_range([0, 0], [1, 0])
+      grid.turn_off([0, 0], [1, 1])
+      grid.turn_on([0, 0], [1, 0])
 
       assert_equal 2, grid.on_count
       assert_equal 2, grid.off_count
@@ -32,8 +32,8 @@ module Christmas
 
     def test_lights_on_two_by_two_vertical_line
       grid = Grid.new(2)
-      grid.turn_off_range([0, 0], [1, 1])
-      grid.turn_on_range([0, 0], [0, 1])
+      grid.turn_off([0, 0], [1, 1])
+      grid.turn_on([0, 0], [0, 1])
 
       assert_equal 2, grid.on_count
       assert_equal 2, grid.off_count
@@ -41,8 +41,8 @@ module Christmas
 
     def test_lights_on_two_by_two_square
       grid = Grid.new(2)
-      grid.turn_off_range([0, 0], [1, 1])
-      grid.turn_on_range([0, 0], [1, 1])
+      grid.turn_off([0, 0], [1, 1])
+      grid.turn_on([0, 0], [1, 1])
 
       assert_equal 4, grid.on_count
       assert_equal 0, grid.off_count
