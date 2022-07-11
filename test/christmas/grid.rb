2b43587fb25999a43d45e2326ec7d33238af6f52
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-07-11 12:09:31 +0400

    refactor two dimensional range

--- test/christmas/grid.rb
+++ test/christmas/grid.rb
@@ -1,6 +1,21 @@
 require "christmas/light"
 
 module Christmas
+  class TwoDimensionalRange
+    def initialize(start, finish)
+      @x1 = start[0]
+      @y1 = start[1]
+      @x2 = finish[0]
+      @y2 = finish[1]
+    end
+
+    def points
+      xs = *(@x1..@x2)
+      ys = *(@y1..@y2)
+      xs.product ys
+    end
+  end
+
   class Grid
     def initialize(size)
       @lights = Array.new(size) { Array.new(size) { Light.new } }
@@ -19,11 +34,10 @@ module Christmas
     end
 
     def apply(start, last, action)
-      x1, y1 = start
-      x2, y2 = last
-      (x1..x2).each do |x|
-        (y1..y2).map { |y| @lights[x][y] }.each(&action)
-      end
+      TwoDimensionalRange.new(start, last)
+                         .points
+                         .map { |(x, y)| @lights[x][y] }
+                         .each(&action)
     end
 
     def on_count
